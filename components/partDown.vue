a59a4355ec687ff5d6ce09581629634ba856e81d
david <ramosdavu2006@gmail.com>
2025-05-21 11:27:03 +0200

    ahora si funciona

--- components/partDown.vue
+++ components/partDown.vue
@@ -1,22 +1,19 @@
 <template>
   <div class="flex pt-5 bg-blue-900 p-4 justify-between">
     <p class="text-white">By Davurg 😎</p>
-    <div class="flex gap-4 items-center ">
-      <a target="_blank" href="https://www.instagram.com/davurg_06/">
-        <NuxtImg src="instagram.svg" width="30"/>
+    <div class="flex gap-4 items-center">
+      <a href="https://www.instagram.com/davurg_06/" target="_blank" rel="noopener noreferrer">
+        <NuxtImg src="instagram.svg" width="30" />
       </a>
-      <a target="_blank" href="https://twitter.com/Davurg_06">
-        <img src="/twitter.png" width="40" alt="twitter"/>
+      <a href="https://twitter.com/Davurg_06" target="_blank" rel="noopener noreferrer">
+        <img src="/twitter.png" width="40" alt="twitter" />
       </a>
-      <a target="_blank" href="https://www.facebook.com/profile.php?id=100094613507843">
-        <NuxtImg src="facebook.svg" width="30"/>
+      <a href="https://www.facebook.com/profile.php?id=100094613507843" target="_blank" rel="noopener noreferrer">
+        <NuxtImg src="facebook.svg" width="30" />
       </a>
-      <a target="_blank" href="https://api.whatsapp.com/send?phone=679219416">
-        <NuxtImg src="whatsapp.svg" width="30"/>
+      <a href="https://api.whatsapp.com/send?phone=679219416" target="_blank" rel="noopener noreferrer">
+        <NuxtImg src="whatsapp.svg" width="30" />
       </a>
     </div>
   </div>
 </template>
-
-<script>
-</script>
