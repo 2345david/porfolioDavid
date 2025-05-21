0c3aee9eefc03f54f9ea2c74ac9b5b6396052cc0
david <ramosdavu2006@gmail.com>
2025-05-21 11:24:43 +0200

    he echo en el footer que al pulsar en los iconos de redes sociales se habra en otra ventana y no en la misma de mi porfolio

--- components/partDown.vue
+++ components/partDown.vue
@@ -2,16 +2,16 @@
   <div class="flex pt-5 bg-blue-900 p-4 justify-between">
     <p class="text-white">By Davurg 😎</p>
     <div class="flex gap-4 items-center ">
-      <a href="https://www.instagram.com/davurg_06/">
+      <a target="_blank" href="https://www.instagram.com/davurg_06/">
         <NuxtImg src="instagram.svg" width="30"/>
       </a>
-      <a href="https://twitter.com/Davurg_06">
+      <a target="_blank" href="https://twitter.com/Davurg_06">
         <img src="/twitter.png" width="40" alt="twitter"/>
       </a>
-      <a href="https://www.facebook.com/profile.php?id=100094613507843">
+      <a target="_blank" href="https://www.facebook.com/profile.php?id=100094613507843">
         <NuxtImg src="facebook.svg" width="30"/>
       </a>
-      <a href="https://api.whatsapp.com/send?phone=679219416">
+      <a target="_blank" href="https://api.whatsapp.com/send?phone=679219416">
         <NuxtImg src="whatsapp.svg" width="30"/>
       </a>
     </div>
